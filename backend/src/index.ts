3fac1cb7791814e0efaf8a6e3cd07e7271af23ba
haseebahmed248 <haseebahmed6157@gmail.com>
2025-09-17 00:40:41 +0500

    TFB-10 feat: Setup PostgreSQL with Sequelize and health check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { connectDB, sequelize } from './config/sequelize.js';
+import dotenv from 'dotenv'
+import { ConnectionError, DatabaseError } from 'sequelize';
 
 
 class App{
@@ -8,25 +11,42 @@ class App{
         this.app = express();
         this.setConfig();
         this.setupRoutes();
-        this.startServer(7000);
     }
 
     private setConfig(): void {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
+        dotenv.config();
     }
 
     setupRoutes(): void{
         this.app.use('/',(_req, res)=>{
             res.json("Hello world")
         })
+        this.app.use('/health-check', async(_req,res)=>{
+            try{
+                await sequelize.query('SELECT 1');
+                res.status(201).json({status: 'ok', db: 'connected'});
+            }catch(err: unknown){
+                if (err instanceof ConnectionError) {
+                    res.status(500).json({ status: "error", db: "connection failed" });
+                } else if (err instanceof DatabaseError) {
+                    res.status(500).json({ status: "error", db: "query failed" });
+                } else if (err instanceof Error) {
+                    res.status(500).json({ status: "error", db: "disconnected", message: err.message });
+                } else {
+                    res.status(500).json({ status: "error", db: "unknown issue" });
+                }
+            }
+        })
     }
 
-    startServer(port: number): void {
+    async startServer(port: number): Promise<void> {
+        await connectDB();
         this.app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
     }
 }
-const app = new App().app;
+const app = new App().startServer(7000);
 export { app };
